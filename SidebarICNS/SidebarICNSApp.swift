023141c0377b10d3b1ff406b1f4106f02173550a
Kayden Tebau <me@nullob.si>
2022-09-30 19:33:07 -0700

    Initial Commit

---
+++ SidebarICNS/SidebarICNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarICNSApp.swift
+//  SidebarICNS
+//
+//  Created by Kayden Tebau on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct SidebarICNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
